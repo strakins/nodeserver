98608f2753cb86efc9fd2c05a8e8678d1c6591ef
strakins <akinolablessing25@gmail.com>
2023-07-25 23:51:20 +0100

    Task 2 - File Manipulation

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const http = require ('http');
+const fs = require('fs')
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    if(req.method === 'GET' && req.url === '/') {
-        res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        })
-        res.end('Hello, Node.js!')
+    if(req.method === 'GET') {
+        if( req.url === '/') {
+            // Print Hello Nodejs to webpage
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            })
+            res.end('Hello, Node.js!')
+        }else if(req.url === '/file') {
+            fs.readFile('data.txt', 'utf-8', (err, data) => {
+                if(err) {
+                    res.writeHead(500, {'Content-Type': 'text/plain'});
+                    res.end('Internal Server Error');
+                }else {
+                    res.writeHead(200, { 'Content-Type': 'text/plain'});
+                    res.end(data)
+                }
+            })
+        }
     }
 });
 
